652792afcd613b278c6ae0785b850e88c5bc84a4
Timothy Gillespie <tigill@uni-koblenz.de>
2019-04-27 21:56:12 +0200

    Add more test cases and adjust the previous one to the new return type

--- src/util/prolog/prolog.test.ts
+++ src/util/prolog/prolog.test.ts
@@ -1,29 +1,105 @@
 import { PrologSession } from "./prolog";
-import { format_answer, type } from "tau-prolog/modules/core";
 
-const pathFile : string = "src/util/prolog/fetch";
+test("Truth Value (True)", async () => {
+		const session = new PrologSession("likes(lisa, bob).", 1000);
+		const result = await session.executeQuery("likes(lisa, bob).");
 
-test("runs at all", async () => {
-    const session = new PrologSession("mag(lisa, bob).", 1000);
-    const result = await session.executeQuery("mag(lisa, bob).");
-
-    expect(result).toEqual(["true ;", "false."]);
+		expect(result).toEqual([["true"], ["false"]]);
 });
 
+test("Truth Value (False)", async () => {
+		const session = new PrologSession("likes(lisa, bob).", 1000);
+		const result = await session.executeQuery("likes(lisa, dieter).");
 
-test("writes", async () => {
-    const session = new PrologSession("mag(lisa, bob). ?- open('" + pathFile +"', append, Stream), write(Stream, 'bar'), close(Stream).", 1000);
+		expect(result).toEqual([["false"]]);
+});
 
-    // expect(result.map(r => typeof r)).toEqual([]);
+test("One Variable One Result", async () => {
+
+		const session = new PrologSession("likes(lisa, bob).", 1000);
+		const result = await session.executeQuery("likes(lisa, X).");
+		expect(result).toEqual([["bob"], ["false"]]);
 
 });
 
-test("prolog code in executeQuery()", async () => {
-    const session = new PrologSession("", 1000);
-    const result = await session.executeQuery(``);
+test("Two Variables One Result", async () => {
+		const session = new PrologSession("likes(lisa, bob, karl).", 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y).");
 
-    // expect(result.map(r => typeof r)).toEqual([]);
+		expect(result).toEqual([["bob", "karl"], ["false"]]);
 
-    expect(result).toEqual(["true ;", "false."]);
+});
 
+test("Three Variables One Result", async () => {
+		const session = new PrologSession("likes(lisa, bob, karl, friedrich).", 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y, Z).");
+
+		expect(result).toEqual([["bob", "karl", "friedrich"], ["false"]]);
+
+});
+
+test("Four Variables One Result", async () => {
+		const session = new PrologSession("likes(lisa, bob, karl, friedrich, dieter).", 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y, Z, A).");
+
+		expect(result).toEqual([["bob", "karl", "friedrich", "dieter"], ["false"]]);
+
+});
+
+test("One Variable Two Results", async () => {
+
+		const session = new PrologSession("likes(lisa, bob). likes(lisa, dieter).", 1000);
+		const result = await session.executeQuery("likes(lisa, X).");
+		expect(result).toEqual([["bob"], ["dieter"], ["false"]]);
+
+});
+
+test("Two Variables Two Results", async () => {
+		const session = new PrologSession("likes(lisa, bob, karl). likes(lisa, karl, bob).", 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y).");
+
+		expect(result).toEqual([["bob", "karl"], ["karl", "bob"], ["false"]]);
+
+});
+
+test("Three Variables Two Results", async () => {
+		const session = new PrologSession("likes(lisa, bob, karl, friedrich). likes(lisa, bob, friedrich, karl).", 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y, Z).");
+
+		expect(result).toEqual([["bob", "karl", "friedrich"], ["bob", "friedrich", "karl"], ["false"]]);
+
+});
+
+test("Four Variables Two Results", async () => {
+		const session = new PrologSession(`
+			likes(lisa, bob, karl, friedrich, dieter).
+			likes(lisa, bob, karl, dieter, friedrich).
+			`, 1000);
+		const result = await session.executeQuery("likes(lisa, X, Y, Z, A).");
+
+		expect(result).toEqual([["bob", "karl", "friedrich", "dieter"], ["bob", "karl", "dieter", "friedrich"], ["false"]]);
+
+});
+
+test("Cut Operator Maximum Function", async () => {
+
+		const session = new PrologSession("max(X,Y,Y)  :-  X  =<  Y, !. max(X, Y, X).", 1000);
+		const result = await session.executeQuery("max(3, 7, X).");
+		expect(result).toEqual([["7"], ["false"]]);
+
+});
+
+test("Cut Operator Endless Loop", async () => {
+
+		const session = new PrologSession("likes(lisa, bob, karl). likes(X, Y, Z):- likes(X, Z, Y), !.", 1000);
+		const result = await session.executeQuery("likes(lisa, Y, Z).");
+		expect(result).toEqual([["bob", "karl"], ["karl", "bob"], ["false"]]);
+
+});
+
+test("Truth Value (True) Perfomance", async () => {
+		const session = new PrologSession("likes(bob, lisa).", 1000);
+		const result = await session.executeQuery("likes(bob, lisa).");
+
+		expect(result).toEqual([["true"], ["false"]]);
 });
