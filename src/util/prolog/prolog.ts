61c0a17a44c4a94773c08aa6de4e4ff9728f5a1a
Timothy Gillespie <tigill@uni-koblenz.de>
2019-04-27 21:55:14 +0200

    Change and implement a different return type to support variables better

--- src/util/prolog/prolog.ts
+++ src/util/prolog/prolog.ts
@@ -1,35 +1,52 @@
 import { format_answer, type } from "tau-prolog/modules/core";
 
 export class PrologSession {
-    public readonly session: any;
+		public readonly session: any;
 
-    constructor(program: string, limit?: number) {
-        this.session = new type.Session(limit);
-        //Loadig the program already fetches writes to a file in case of open -> write -> close
-        this.session.consult(program);
-    }
+		constructor(program: string, limit?: number) {
+				this.session = new type.Session(limit);
+				// Loadig the program already fetches writes to a file in case of open -> write -> close
+				this.session.consult(program);
+		}
 
-    public async executeQuery(query: string): Promise<string[]> {
-        this.session.query(query);
+		public async executeQuery(query: string): Promise<string[][]> {
+				this.session.query(query);
 
-        const results: string[] = [];
+				const results: string[][] = [];
 
-        let result: string;
-        do {
-            result = await this.getNextAnswer();
-            results.push(result);
-        } while (result[result.length - 1] !== ".");
-        //this could be checked more reliably with the this.session.step() method
-        //it returns an undefined if the end of the threat has been reached, but it does a step when called
+				let result: string;
+				do {
+						result = await this.getNextAnswer();
+						this.formatMultiArray(result, results);
+				} while (result[result.length - 1] !== ".");
+				// this could be checked more reliably with the this.session.step() method
+				// it returns an undefined if the end of the threat has been reached, but it does a step when called
 
-        return results;
-    }
+				return results;
+		}
 
-    private getNextAnswer() {
-        return new Promise<string>((resolve, reject) => {
-            this.session.answers((x: any) => {
-                resolve(format_answer(x));
-            });
-        });
-    }
+		private getNextAnswer() {
+				return new Promise<string>((resolve, reject) => {
+						this.session.answers((x: any) => {
+								resolve(format_answer(x));
+						});
+				});
+		}
+
+		private formatMultiArray(source: string, goal: string[][]): void {
+			goal.push([]);
+			// There is probably a more elegant way to fetch these cases
+			if (source.includes("true")) { goal[goal.length - 1][0] = "true"; return; }
+			if (source.includes("false")) { goal[goal.length - 1][0] = "false"; return; }
+
+			const pattern: any = /= (\w*|\[.*\])?(,| ;|.)/g;
+			const components: any = source.match(pattern);
+
+			components.forEach((e: any) => {
+				// Throws out the first two chars (= and a blank) and the last char (either , ; or .)
+				const toAdd: string = e.substring(2, e.length - 1).trim();
+
+				goal[goal.length - 1].push(toAdd);
+			});
+		}
 }
