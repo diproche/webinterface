a42798ddb2b23ea771f1cc578c481a42b3c2cb36
Timothy Gillespie <tigill@uni-koblenz.de>
2019-05-02 09:32:52 +0200

    Add functionality and tests for nested arrays which don't work yet.

---
+++ src/util/nestedArray/nestedArray.ts
@@ -0,0 +1,22 @@
+export function nestedIndexOf(hay: any[][], needle: any[]): number {
+	let i = 0;
+
+	hay.forEach((e: any) => {
+		if (e === needle { return i; }
+		i = i + 1;
+	});
+
+	return -1;
+}
+
+/*
+Array.prototype.nestedIndexOf = (search: any[]) => {
+	let i = 0;
+
+	this.forEach((e: any) => {
+		if (e === search) { return i; }
+		i = i + 1;
+	});
+
+};
+*/
