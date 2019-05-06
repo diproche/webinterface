387cd1253883d279ad1c973e12690bf54840d981
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-05-06 15:10:26 +0200

    Version 0.2: improved input detector and some fixes

--- src/text_formatter/text_formatter.test.tsx
+++ src/text_formatter/text_formatter.test.tsx
@@ -5,45 +5,51 @@ import ReactDOM from "react-dom";
 // @author: Ronja K.
 // ---------------------------------------
 // IMPORT FUNCTIONS
-import {textToListFormat, sentenceIntoWordList, functionFormatter,removeWhiteSpaces} from "./text_formatter";
+//import { detectExpressionElements, textToListFormat, sentenceIntoWordList, expressionFormatter, removeWhiteSpaces, replaceInputCaractersToReadablePrologCharacter } from "./text_formatter";
+import { textToListFormat, sentenceIntoWordList, detectExpressionElements, replaceInputCaractersToReadablePrologCharacter, removeWhiteSpaces, expressionFormatter } from "./text_formatter";
 
 // ---------------------------------------
 // TESTS FOR SPLITTING TEXT
 // ---------------------------------------
-/*test("Test splitting full text into List of sentences: ", () => {
-	const result = textToSentencesList("Hello, this is a test! Is it Working? I hope so.");
-	const expectedResult = ["Hello, this is a test", "Is it Working", "I hope so"];
-	expect(result).toEqual(expectedResult);
-});*/
 
-test("Test splitting a single sentences into List of words: ", () => {
+test("Test: splitting a single sentences into List of words: ", () => {
 	const result = sentenceIntoWordList("Hello, this is a test");
 	const expectedResult = ["Hello", "this", "is", "a", "test"];
 	expect(result).toEqual(expectedResult);
 });
 
 
-test("Test splitting full text into full listFormat excluding expressions: ", () => {
-	const result = textToListFormat("Hello, this is a test! Is it Working? I               hope so. \n Here is also a expression: $[A UND [B <==> C] -> D ODER NOT E]$");
-	const expectedResult = [["Hello", "this", "is", "a", "test"], ["Is", "it", "Working"], ["I", "hope", "so"],["abs"], ["Here", "is", "also", "a", "expression:"],["[", "A", "UND", "[", "B", "<==>", "C", "]", "->", "D", "ODER", "NOT", "E", "]"]];
-	//expect(result).toEqual(expectedResult);
-	expect(result).toEqual(expectedResult);
-});
 
 // ---------------------------------------
 // TESTS FOR EXPRESSIONS-LOGIK
 // ---------------------------------------
+test("Test if the elements of a expression is detected correctly:", () => {
+	const result = detectExpressionElements("[AUNDTest[B<==>C]->DODERNOTNOTE]");
+	const expectedResult = " bracketLeft A conjunction Test bracketLeft B equivalence C bracketRight  implication D disjunction  negation  negation E bracketRight ";
+	expect(result).toEqual(expectedResult);
+});
+
+test("replace detected expression-elements into readable prolog commands", () => {
+	const result = replaceInputCaractersToReadablePrologCharacter("bracketLeft A conjunction RANDOMTEXTHERE bracketLeft B equivalence C bracketRight bracketRight");
+	const expectedResult = "[ A and RANDOMTEXTHERE [ B <-> C ] ]";
+	expect(result).toEqual(expectedResult);
+});
 
 
-test("Test white Space remover between expressions.", () => {
+
+
+test("Test: white Space remover between expressions.", () => {
 	const result = removeWhiteSpaces("(5 + 6       = 11=>89F  allFOR, orf <-> test*h)");
 	const expectedResult = "(5+6=11=>89FallFOR,orf<->test*h)";
 	expect(result).toEqual(expectedResult);
 });
 
-// TO DO
-test("Test if a expression is splitted correctly: [A UND B <==> C -> D ODER NOT E]]", () => {
-	const result = functionFormatter("[A UND [B<==>C  ]   ->D ODER NOTE]");
-	const expectedResult = ["[", "A", "UND", "[", "B", "<==>", "C", "]", "->", "D", "ODER", "NOT", "E", "]"];
+test("Test: splitting full text into full listFormat including expressions and paragraph marker: ", () => {
+	const result = textToListFormat("Hello, this is a test! Is it Working? I               hope so. \n Here is also a expression: $[A UND [B <==> C] -> D ODER NOT E]$");
+	const expectedResult = [["Hello", "this", "is", "a", "test"], ["Is", "it", "Working"], ["I", "hope", "so"], ["abs"], ["Here", "is", "also", "a", "expression:"], ["[", "A", "and", "[", "B", "<->", "C", "]", "->", "D", "or", "neg", "E", "]"]];
 	expect(result).toEqual(expectedResult);
 });
+
+
+//-------TO DO: count if amount of brackets is correct---------
+//-------TO DO: format expressions into prolog format----------
--- src/text_formatter/text_formatter.ts
+++ src/text_formatter/text_formatter.ts
@@ -8,98 +8,171 @@ import { ProofEditor } from "../components/proofEditor/proofEditor";
 // @author: Ronja K.
 // ---------------------------------------
 
+// ---------format the input of a user into a full list of senteces including a list of words, formatted expressions, and paragraph marker-------
 export function textToListFormat(input: String) {
 
-    input = input.replace("$", "$EXPRESSIONMARKER");                              //prepare expressionsdetector; this is needed as marker to finde the expressions after the $ disappear by calling the split-method
-    input = input.replace("\n", "\n PARAGRAPHMARKER.");                         //prepare paragraphmarker
+	input = input.replace("$", "$EXPRESSIONMARKER");                            // prepare expressionsdetector; this is needed as marker to finde the expressions after the $ disappear by calling the split-method
+	input = input.replace("\n", "\n PARAGRAPHMARKER.");                         // prepare paragraphmarker
 
-    const separator = /[\n.!?$]+/;                                              // List all the values the input will get splitted; \n will split paragraphs, $ will split expressions, the rest split sentences
+	const separator = /[\n.!?$]+/;                                              // List all the values the input will get splitted; \n will split paragraphs, $ will split expressions, the rest split sentences
 
-    const splittedText = input.split(separator);
-    const temp = [];
-    //let sentenceCount = 0;                                                  //  |optional
-    //let expressionCount = 0;                                                //  |optional
-    //let paragraphCount = 0;                                                 //  |optional
-    for (let index = 0; index < splittedText.length; index++) {
-        var element = splittedText[index].trim();                               // whitespaces between sentences are not needed
-        if (element != "") {
+	const splittedText = input.split(separator);
+	const temp = [];
+	// let sentenceCount = 0;                                                  //  |optional
+	// let expressionCount = 0;                                                //  |optional
+	// let paragraphCount = 0;                                                 //  |optional
+	for (let index = 0; index < splittedText.length; index++) {
+		let element = splittedText[index].trim();                               // whitespaces between sentences are not needed
+		if (element != "") {
 
-            if (element.match(/(EXPRESSIONMARKER)/)) {
-                //temp.push("Expression_" + (expressionCount + 1));           //  |optional
-                //expressionCount++;                                          //  |optional
-                element = element.replace("EXPRESSIONMARKER", "");
-                const formattedExpression = functionFormatter(element);
-                temp.push(formattedExpression);
-            }
+			if (element.match(/(EXPRESSIONMARKER)/)) {						   // DETECT AND ADD EXPRESSIONS TO LIST
+				// temp.push("Expression_" + (expressionCount + 1));           //  |optional
+				// expressionCount++;                                          //  |optional
+				element = element.replace("EXPRESSIONMARKER", "");
+				const formattedExpression = expressionFormatter(element);
+				temp.push(formattedExpression);
+			} else if (element.match(/(PARAGRAPHMARKER)/)) {				   // DETECT AND ADD PARAGRAPH TO LIST
+				// temp.push("Paragraph_" + (paragraphCount + 1));             // |optional
+				// paragraphCount = 0;                                         // |optional
+				element = element.replace("PARAGRAPHMARKER", "abs");
+				const ListWithparagraph = [];										// store paragraph marker in a own list
+				ListWithparagraph.push(element);
+				temp.push(ListWithparagraph);
+			} else {															// DETECT AND ADD WORDS TO LIST
+				// temp.push("Sentence_" + (sentenceCount + 1));                // |optional
+				// sentenceCount++;                                             // |optional
+				const ListOfWords = sentenceIntoWordList(element);
+				temp.push(ListOfWords);
+			}
 
-            else if (element.match(/(PARAGRAPHMARKER)/)) {
-                //temp.push("Paragraph_" + (paragraphCount + 1));             // |optional
-                //paragraphCount = 0;                                         // |optional
-                element = element.replace("PARAGRAPHMARKER", "abs")
-                const paragraph = []
-                paragraph.push(element);
-                temp.push(paragraph);
-            }
-            else {
-                //temp.push("Sentence_" + (sentenceCount + 1));                // |optional
-                //sentenceCount++;                                             // |optional
-                const sentenceIntoWords = sentenceIntoWordList(element);
-                temp.push(sentenceIntoWords);
-            }
+		}
 
-        }
-
-    }
-    const listFormat = temp;
-
-    return listFormat;
+	}
+	const listFormat = temp;
+	return listFormat;
 }
 
+// ---------format the words from a sentence-string into a list of words-------
 export function sentenceIntoWordList(input: String) {
-    const separator = /[ ,$]+/; // List all the values where the content of a sentence will get splitted: whitespace[ ] and comma[,]
-    const splittedSentenceIntoListOfWords = input.split(separator);
-    const temp = [];
-    let index = 0;
-    for (let index = 0; index < splittedSentenceIntoListOfWords.length; index++) {
-        // remove all elements(senteces) which are a empty String after splitting
-        if (splittedSentenceIntoListOfWords[index] == "") {
-            splittedSentenceIntoListOfWords.splice(index, 1);
-        } else {
-            let string = splittedSentenceIntoListOfWords[index].trim();
-            string.trim();
-            splittedSentenceIntoListOfWords[index] = string;
-            // temp.push("Word_" + (index + 1));
-            temp.push(splittedSentenceIntoListOfWords[index]);
-        }
-    }
-    const ListOfWords = temp;
-    return ListOfWords;
+	const separator = /[ ,$]+/; // List all the values where the content of a sentence will get splitted: whitespace[ ] and comma[,]
+	const splittedSentenceIntoListOfWords = input.split(separator);
+	const temp = [];
+	const index = 0;
+	for (let index = 0; index < splittedSentenceIntoListOfWords.length; index++) {
+		// remove all elements(senteces) which are a empty String after splitting
+		if (splittedSentenceIntoListOfWords[index] == "") {
+			splittedSentenceIntoListOfWords.splice(index, 1);
+		} else {
+			const word = splittedSentenceIntoListOfWords[index].trim();
+			word.trim();
+			splittedSentenceIntoListOfWords[index] = word;
+			temp.push(splittedSentenceIntoListOfWords[index]);
+		}
+	}
+	const ListOfWords = temp;
+	return ListOfWords;
 
 }
+export function expressionFormatter(input: string) {
+	const detectedExpression = detectExpressionElements(input);
+	const separators = /(bracketLeft|bracketRight|equivalence|implication|negation|conjunction|disjunction)+/g;
+	const splittedExpression = detectedExpression.split(separators);
+	const temp = [];
+	for (let index = 0; index < splittedExpression.length; index++) {
+		let tempString = removeWhiteSpaces(splittedExpression[index]);
+		if (tempString !== "") {
+			tempString = replaceInputCaractersToReadablePrologCharacter(tempString);
+			temp.push(tempString);
+		}
+	}
+	const finalExpressionArray = temp;
+	return finalExpressionArray;
+}
 
-export function functionFormatter(input: string) {
-    const splittedInput = splitFunction(input); // split function at "="
-    return splittedInput;
+// ---------CurrentExpressionDetector------------
+// ---------every element have their own detect-function--------
+export function detectExpressionElements(input: string) {
+	input = detectBracketLeft(input);
+	input = detectBracketRight(input);
+	input = detectEquivalence(input);
+	input = detectImplication(input);
+	input = detectImplication(input);
+	input = detectNegation(input);
+	input = detectConjunction(input);
+	input = detectDisjunction(input);
+	return input;
 }
 
 export function removeWhiteSpaces(input: string) {
-    const output = input.replace(/\s+/g, ""); // delete all white spaces
-    return output;
+	const output = input.replace(/\s+/g, ""); // delete all white spaces
+	return output;
 }
 
-export function splitFunction(input: string) {
 
-    const separators = /(|<-->|<=>|<==>|->|-->|=>|<--|<-|<=|UND|AND|ODER|OR|NOT|NICHT)+/g;
-    let splittedExpression = input.split(separators);
-    const temp = [];
-    for (let index = 0; index < splittedExpression.length; index++) {
-        var string = removeWhiteSpaces(splittedExpression[index]);
-        if (string !== "") {
-            temp.push(splittedExpression[index]);
-        }
 
-    }
 
-    splittedExpression = temp;
-    return splittedExpression;
+export function detectBracketLeft(input: string) {
+	input = input.replace(/(\[|\()/g, " bracketLeft ");
+	return input;
 }
+
+export function detectBracketRight(input: string) {
+	input = input.replace(/(\]|\))/g, " bracketRight ");
+	return input;
+}
+
+export function detectEquivalence(input: string) {
+
+	input = input.replace(/(<-->|<==>|<=>|<->)/g, " equivalence ");
+	return input;
+}
+
+export function detectImplication(input: string) {
+	input = input.replace(/(->|-->|=>|==>)/g, " implication ");
+	return input;
+}
+
+export function detectNegation(input: string) {
+	input = input.replace(/(neq|NEQ|Neq|NOT|Not|not|NICHT|Nicht|nicht|¬)/g, " negation ");
+	return input;
+}
+
+export function detectConjunction(input: string) {
+	input = input.replace(/(and|And|AND|und|Und|UND|&|∧)/g, " conjunction ");
+	/*input = input.replace(/And/g, " conjunction ");
+	input = input.replace(/AND/g, " conjunction ");
+	input = input.replace(/und/g, " conjunction ");
+	input = input.replace(/Und/g, " conjunction ");
+	input = input.replace(/UND/g, " conjunction ");
+	input = input.replace(/&/g, " conjunction ");
+	input = input.replace(/∧/g, " conjunction ");
+	*/
+	return input;
+}
+
+export function detectDisjunction(input: string) {
+	input = input.replace(/(or|Or|OR|oder|Oder|ODER|\||∨)/g, " disjunction ");
+	/*	input = input.replace(/Or/g, " disjunction ");
+		input = input.replace(/OR/g, " disjunction ");
+		input = input.replace(/oder/g, " disjunction ");
+		input = input.replace(/Oder/g, " disjunction ");
+		input = input.replace(/ODER/g, " disjunction ");
+		input = input.replace(/[|]+/g, " disjunction ");
+		input = input.replace(/∨/g, " disjunction ");
+		*/
+	return input;
+}
+
+// --------replace expression elements into readable prolog Code---------
+export function replaceInputCaractersToReadablePrologCharacter(input: string) {
+	//input = detectExpressionElements(input);
+	input = input.replace(/(bracketLeft)/g, "\[");
+	input = input.replace(/(bracketRight)/g, "\]");
+	input = input.replace(/(equivalence)/g, "<->");
+	input = input.replace(/(implication)/g, "->");
+	input = input.replace(/(negation)/g, "neg");
+	input = input.replace(/(conjunction)/g, "and");
+	input = input.replace(/(disjunction)/g, "or");
+	return input;
+}
+
