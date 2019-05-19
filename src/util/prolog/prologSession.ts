ab1e77cf52e158b58c830e7ac6e01bf4f3fd29cf
Timothy Gillespie <tigill@uni-koblenz.de>
2019-05-19 19:23:42 +0200

    Add import functionality for prolog files

--- src/util/prolog/prologSession.ts
+++ src/util/prolog/prologSession.ts
@@ -1,12 +1,32 @@
+import fs from "fs";
+import path from "path";
 import { format_answer, type } from "tau-prolog/modules/core";
 import {PrologResult} from "./prologResult";
+
+// RegExp group one will be the file to be imported. Note: not global
+const fetchPrologImportsRegExp = /:-( |)use_module\(([a-zA-Z1-9]*)\)\./;
+
+export function importFile(relativePath: string): PrologSession {
+	const absolutePath = path.resolve(__dirname, relativePath);
+	const program = fs.readFileSync(absolutePath, "utf-8");
+	return new PrologSession(program, path.dirname(absolutePath));
+}
 
 export class PrologSession {
 
 		public readonly session: any;
 
-		constructor(program: string, limit?: number) {
+		constructor(program: string, defaultPath: string = __dirname, limit?: number) {
 				this.session = new type.Session(limit);
+
+				let matchGroups: string[] | null;
+				let importedCode: string;
+
+				while ((matchGroups = fetchPrologImportsRegExp.exec(program)) !== null) {
+					 importedCode = fs.readFileSync(defaultPath + path.sep + matchGroups[2] + ".pl", "utf-8");
+					 program = program.replace(fetchPrologImportsRegExp, importedCode);
+				}
+
 				this.session.consult(program);
 		}
 
