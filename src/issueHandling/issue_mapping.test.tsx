ab1d771b1f030c9b2a9db6c1683e40d80090dfa7
Fabian Grewing <Fabian.Heimbuerger@gmail.com>
2019-05-27 20:24:24 +0100

    Export issue-json-key type as IssueCode

--- src/issueHandling/issue_mapping.test.tsx
+++ src/issueHandling/issue_mapping.test.tsx
@@ -1,38 +1,31 @@
 
-import Issue from "./issue";
-import { addIssue, emptyIssueList, getIssue, listAllIssues } from "./issue_mapping";
-import { ISSUES} from "./issue_mapping";
+import { addIssue, emptyIssueList, listAllIssues } from "./issue_mapping";
 
 test("check if the issuelist contains a searched issue but it doesn't", () => {
 	emptyIssueList();
-	addIssue(ISSUES.BRACKET_OVERCLOSING);
-	const issue: Issue = getIssue(ISSUES.BRACKET_UNDERCLOSING);
-	expect(issue).toEqual({
-		code: "ISSUE_NOT_FOUND",
-		message: "The searched Issue wasn't found.",
-		severity: "FATAL_ERROR",
-	});
+	addIssue("BRACKET_OVERCLOSING");
+	const issue = listAllIssues().find(i => i.code === "BRACKET_UNDERCLOSING");
+	expect(issue).toEqual(undefined);
 });
 
 test("check if the issuelist contains a searched issue", () => {
 	emptyIssueList();
 	/** add first issue: */
-	addIssue(ISSUES.BRACKET_OVERCLOSING);
+	addIssue("BRACKET_OVERCLOSING");
 	/** add second issue: */
-	addIssue(ISSUES.MISSING_CONNECTOR);
+	addIssue("MISSING_CONNECTOR");
 	/** add third issue */
-	addIssue(ISSUES.BRACKET_UNDERCLOSING);
+	addIssue("BRACKET_UNDERCLOSING");
 	/** add fourth issue */
-	addIssue(ISSUES.MISSING_STATEMENT_INSIDE);
+	addIssue("MISSING_STATEMENT_INSIDE");
 	/** add fifth issue */
-	addIssue(ISSUES.MISSING_STATEMENT_AT_THE_END);
+	addIssue("MISSING_STATEMENT_AT_THE_END");
 
-	const issue = getIssue(ISSUES.MISSING_STATEMENT_INSIDE);
+	const issue = listAllIssues().find(i => i.code === "MISSING_STATEMENT_INSIDE");
 
 	expect(issue).toEqual({
 		code: "MISSING_STATEMENT_INSIDE",
 		message: "It seems, you forgot some statements inside your logic expression.",
 		severity: "WARNING",
 	});
-
 });
