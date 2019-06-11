5ee546dc07a86b1838576e8e984f2344a959f6b2
Fabian Heimbürger <Fabian.Heimbuerger@gmail.com>
2019-06-11 11:21:12 +0200

    Implement placeholder replacement

--- src/issueHandling/issueMapping.ts
+++ src/issueHandling/issueMapping.ts
@@ -5,6 +5,10 @@ import Position from "./position";
 /** All the valid issue-codes */
 export type IssueCode = keyof typeof issueJson;
 
+interface ReplacementMap {
+	[placeholder: string]: string | number;
+}
+
 /**
  * This function needs to be called when someone wants to store a known issue;
  * @param issueJson the issueobject that should be stored in the issuelist.
@@ -14,13 +18,30 @@ export type IssueCode = keyof typeof issueJson;
 export function addIssue(
 	issueCode: IssueCode,
 	position?: Position,
-	replacements?: { [placeholder: string]: string | number }) {
+	replacements?: ReplacementMap) {
 	const issue: Issue = {
 		...issueJson[issueCode],
 		code: issueCode,
 		position,
 	};
+	if (replacements) {
+		issue.message = insertReplacementsForPlaceholders(issue.message, replacements);
+	}
+
 	addIssueToIssueList(issue);
+}
+
+const PLACEHOLDER_PATTERN = /{([A-Za-z0-9]+)}/g;
+
+function insertReplacementsForPlaceholders(message: string, replacements: ReplacementMap): string {
+	return message.replace(PLACEHOLDER_PATTERN, (placeholder, name) => {
+		const replacement = replacements[name];
+		if (replacement === undefined) {
+			return placeholder;
+		} else {
+			return replacement.toString();
+		}
+	});
 }
 
 let foundIssues: Issue[] = [];
