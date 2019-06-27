290ddce11dd7d73d260929e71bca498e042ac1ae
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-06-27 10:01:58 +0200

    Implement react router and some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,63 @@
 import React, { Component } from "react";
-import { MainPage } from "./components/GUI/mainPage/mainPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navigation from "./components/GUI/navigation/navigation";
+import ProofEditor from "./components/GUI/proofEditor/proofEditor";
 
 class App extends Component {
 	public render() {
-		window.location.href = "#startseite";
+
 		return (
-			<div className="App">
-			<MainPage />
-			</div>
+			<BrowserRouter>
+				<React.Fragment>
+					<Navigation />
+					<Switch>
+						<Route path="/" component={Startseite} exact />
+						<Route path="/aussagenlogisches_beweise" component={ProofEditor} exact />
+						<Route path="/einstellungen" component={Einstellungen} exact />
+						<Route path="/kontakt" component={Kontakt} exact />
+						<Route path="/impressum" component={Impressum} exact />
+					</Switch>
+				</React.Fragment>
+			</BrowserRouter>
 		);
 
 	}
 
 }
+
+// These are just demonstration pages. Please don't implement the pages here.
+// Implement them as external components which will be imported
+
+const Startseite = () => {
+	return (
+		<div>
+			<p>Startseite</p>
+		</div>
+	);
+};
+
+const Einstellungen = () => {
+	return (
+		<div>
+			<p>Einstellungen</p>
+		</div>
+	);
+};
+
+const Kontakt = () => {
+	return (
+		<div>
+			<p>Kontakt</p>
+		</div>
+	);
+};
+
+const Impressum = () => {
+	return (
+		<div>
+			<p>Impressum</p>
+		</div>
+	);
+};
 
 export default App;
--- src/components/GUI/proofEditor/proofEditor.tsx
+++ src/components/GUI/proofEditor/proofEditor.tsx
@@ -2,8 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import Issue from "../../../issueHandling/issue";
 import { checkProof } from "../../../util/proofChecker";
-import mainLayout from "../../CSS/mainLayout.module.scss";
-import navBar from "../../CSS/navBar.module.scss";
 import styles from "../../CSS/proofEditor.module.scss";
 import IssueInformation from "./issueInformation";
 
@@ -16,27 +14,14 @@ interface State {
 	issues: readonly Issue[];
 }
 
-export class ProofEditor extends React.Component<Props, State> {
+class ProofEditor extends React.Component<Props, State> {
 	public state = {
 		userInput: "",
 		issues: [],
-		name: "Aussagenlogisches Beweisen",
-		href: "#aussagenlogisches_beweisen",
 	};
 
 	public render() {
-		// return SiteLayout;
-		window.location.href = "#aussagenlogisches_beweisen";
-		return <div className={mainLayout.page}>
-			<div className={navBar.name}>
-				<ul>
-					<li><a className="backToMain" href="#startseite">Zurück zur Startseite</a></li>
-					<li><a className="settings" href="#einstellungen">Einstellungen</a></li>
-					<li><a className="contact" href="#kontakt">Kontakt</a></li>
-					<li><a className="impressum" href="#impressum">Impressum</a></li>
-				</ul>
-			</div>
-			<div className={styles.proofEditor}>
+		return <div className={styles.proofEditor}>
 				<textarea
 					className={styles.textInput}
 					placeholder="Gebe hier deinen Beweis ein."
@@ -46,10 +31,8 @@ export class ProofEditor extends React.Component<Props, State> {
 				<button className={styles.buttons}
 					onClick={this.checkInput}>
 					Prüfen
-			</button>
-
+				</button>
 				<div className={styles.issuesInformation}>
-
 					{this.state.issues.map((issue: Issue) => {
 						return <IssueInformation
 							issue={issue} />;
@@ -57,7 +40,6 @@ export class ProofEditor extends React.Component<Props, State> {
 					})}
 				</div>
 
-			</div>
 		</div>;
 
 	}
@@ -67,3 +49,5 @@ export class ProofEditor extends React.Component<Props, State> {
 		this.setState({ issues: issueArray });
 	}
 }
+
+export default ProofEditor;
