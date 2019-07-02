7a5bf0ecd983f999fa727684fb72088c1b12868f
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-07-02 14:03:55 +0200

    Correct the JSDoc for fetchAllMatchesForAGroup

--- src/util/regExpUtils.ts
+++ src/util/regExpUtils.ts
@@ -1,15 +1,15 @@
 /**
 	* Fetches a RegExp's group's values by its number for all matches
-	* @param {string} textBasis - The text to be fetched from
-	* @param {RegExp} - The RegExp pattern with groups to be used
-	* @param {number} - Which group number to fetch
-	* @return {Array<string>} All values for this group
+	* @param textBasis - The text to be fetched from
+	* @param pattern - The RegExp pattern with groups to be used
+	* @param group - Which group number to fetch
+	* @return All values for this group
 	*/
-export function fetchAllMatchesForAGroup(basis: string, pattern: RegExp, group: number): string[] {
+export function fetchAllMatchesForAGroup(textBasis: string, pattern: RegExp, group: number): string[] {
 	const groupContent: string[] = [];
 	let matchGroups: string[] | null;
 
-	while ((matchGroups = pattern.exec(basis)) !== null) {
+	while ((matchGroups = pattern.exec(textBasis)) !== null) {
 		groupContent.push(matchGroups[group]);
 	}
 
