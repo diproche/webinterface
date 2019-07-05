f98ed317ecc7ee34ac7ce40d2bf0894892eab371
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-05 16:20:49 +0200

    Create tutorialPage.tsx

---
+++ src/components/proofEditor/tutorialPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ProofEditor extends React.Component {
+
+	public render() {
+		return <div className={"addStyleHere"}>
+			<p>Hier sollte ein Tutorial stehen</p>
+		</div>;
+
+	}
+
+}
+
+export default ProofEditor;
