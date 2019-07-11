142db44b6aa5eccddb276530444b8512b7ad8040
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-07-11 16:13:28 +0200

    Remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"; // This should be lower case, but it forces the uppercase
+import App from "./App";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 
