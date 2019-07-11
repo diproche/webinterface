9407af07a692a3b149cabe1ff85cf8594a0e0cd8
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-11 17:39:27 +0200

    changed sitelayout

--- src/components/propositionalLogic/exercises.tsx
+++ src/components/propositionalLogic/exercises.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 
 import { Exercise, generateRandomExercise } from "./exerciseGenerator";
+import ProofEditor from "./proofEditor";
 import styles from "./propositionalLogic.module.scss";
 
 class ExercisesPropositionalLogic extends React.Component {
 
 	public render() {
 		return <div className={styles.site}>
-			<p>Hier sollten Übungen stehen. Entweder Zufällig generierte bzw. ausgewählte Aufgaben (nach schiwerigkeitsgrad?)
+			<div>
+				<p>Hier sollten Übungen stehen. Entweder Zufällig generierte bzw. ausgewählte Aufgaben (nach schiwerigkeitsgrad?)
 				 oder festes schema)</p>
-			<p>Click the button to display a random exercise.</p>
+				<p>Click the button to display a random exercise.</p>
+				<button className={styles.buttons}
+					onClick={this.checkInput}>
+					Zufallsgenerator
+					</button>
 
-			<button className={styles.buttons}
-				onClick={this.checkInput}>
-				Zufallsgenerator
-	</button>
-			<p id="exerciseDescription"></p>
-			<p id="exerciseContent"></p>
-		</div>;
+				<p id="exerciseDescription"></p>
+				<p id="exerciseContent"></p>
+			</div>
+			<ProofEditor />
+		</div >;
 
 	}
 	private checkInput = async (): Promise<void> => {
