642b3a1a60dccf1a59b121b3f8c7f119b2f07d19
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-11 13:27:24 +0200

    tslint disable ordered imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+// tslint:disable: ordered-imports
+import MainPage from "./components/mainPage/mainPage";
+import Navigation from "./components/navigationBar/navigationBar";
+import Einstellungen from "./components/einstellungen/einstellungen";
+import Kontakt from "./components/kontakt/kontakt";
+import Impressum from "./components/impressum/impressum";
+import ErrorPage404 from "./components/errorPage404/errorPage404";
+
 import ExamplesBooleanAlgebra from "./components/booleanAlgebra/examples";
 import ExercisesBooleanAlgebra from "./components/booleanAlgebra/exercises";
 import FreeProverBooleanAlgebra from "./components/booleanAlgebra/freeProver";
 import NavigationBooleanAlgebra from "./components/booleanAlgebra/navigationBooleanAlgebra";
 import TutorialBooleanAlgebra from "./components/booleanAlgebra/tutorial";
 import WikiBooleanAlgebra from "./components/booleanAlgebra/wiki";
-import Einstellungen from "./components/einstellungen/einstellungen";
-import ErrorPage404 from "./components/errorPage404/errorPage404";
-import Impressum from "./components/impressum/impressum";
-import Kontakt from "./components/kontakt/kontakt";
-import MainPage from "./components/mainPage/mainPage";
-import Navigation from "./components/navigationBar/navigationBar";
+
 import ExamplesPropositionalLogic from "./components/propositionalLogic/examples";
 import ExercisesPropositionalLogic from "./components/propositionalLogic/exercises";
 import FreeProverPropositionalLogic from "./components/propositionalLogic/freeProver";
 import NavigationPropositionalLogic from "./components/propositionalLogic/navigationPropositionalLogic";
 import TutorialPropositionalLogic from "./components/propositionalLogic/tutorial";
 import WikiPropositionalLogic from "./components/propositionalLogic/wiki";
+// tslint:enable: ordered-imports
 
 class App extends Component<{}, {}> {
 	public render() {
