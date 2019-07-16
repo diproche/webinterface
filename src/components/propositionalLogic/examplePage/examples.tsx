81f3289f3b9e3152b7e555dd6eb41499f9869e3b
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-16 14:38:28 +0200

    Update examples.tsx

--- src/components/propositionalLogic/examplePage/examples.tsx
+++ src/components/propositionalLogic/examplePage/examples.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 
+import ProofEditor from "../proofEditor";
+import { Example, generateRandomExample } from "./exampleCollection";
 import styles from "./propositionalLogic.module.scss";
 
 class ExamplesPropositionalLogic extends React.Component {
 
 	public render() {
 		return <div className={styles.site}>
-			<p>Hier sollten Beispiele stehen :)</p>
+			<div>
+				<p>Hier sollten Beispiele stehen. Entweder Zufällig generierte bzw. ausgewählte Aufgaben (nach schiwerigkeitsgrad?)
+				 oder festes schema)</p>
+				<p>Click the button to display a random example.</p>
+				<button className={styles.buttons}
+					onClick={this.checkInput}>
+					Zufallsgenerator
+					</button>
 
-		</div>;
+				<p id="exampleDescription"></p>
+				<p id="exampleContent"></p>
+			</div>
+			<ProofEditor />
+		</div >;
 
 	}
-}
+	private checkInput = async (): Promise<void> => {
+		const exampleDiscription = document.getElementById("exampleDescription");
+		const exampleContent = document.getElementById("exampleContent");
+		const randomExample: Example = generateRandomExample();
+		if (exampleDiscription !== null && exampleContent !== null) {
+			exampleDiscription.innerHTML = randomExample.exampleDiscription;
+			exampleContent.innerHTML = randomExample.exampleContent;
+		}
+	}
 
+}
 export default ExamplesPropositionalLogic;
