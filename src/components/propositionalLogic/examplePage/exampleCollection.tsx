e2e678d481a2cf2b9bcf87aae62c66df0e4dea69
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-16 14:51:42 +0200

    first long example

--- src/components/propositionalLogic/examplePage/exampleCollection.tsx
+++ src/components/propositionalLogic/examplePage/exampleCollection.tsx
@@ -41,3 +41,58 @@ function generateExampleContent(exampleClassNumber: number): string {
 	const test = exampleClassNumber.toString();
 	return test;
 }
+
+
+
+
+/*
+const example = {"
+Wir zeigen (a UND [b ODER c]) <-> ([a UND b] ODER [a UND c]).
+
+=>.
+Angenommen es gilt $(a UND [b ODER c])$.
+Dann gilt a.
+Ferner gilt $b ODER c$.
+Fallunterscheidung.
+Fall 1.
+Angenommen es gilt b.
+Dann gilt $a UND b$.
+Also folgt $([a UND b] ODER [a UND c])$.
+qed.
+Damit gilt $b -> ([a UND b] ODER [a UND c])$.
+Fall 2.
+Angenommen es gilt c.
+Dann gilt $a UND c$,
+Also folgt $([a UND b] ODER [a UND c])$.
+qed.
+Damit gilt $c -> ([a UND b] ODER [a UND c])$.
+In jedem Fall gilt $([a UND b] ODER [a UND c])$.
+Also gilt $([a UND b] ODER [a UND c])$.
+qed.
+Also gilt $([a UND b] ODER [a UND c]) -> (a UND [b ODER c])$.
+
+<=.
+Angenommen es gilt $([a UND b] ODER [a UND c])$.
+Fallunterscheidung.
+Fall 1.
+Angenommen es gilt $a UND b$.
+Dann gilt a.
+Ferner gilt b.
+Also gilt auch $b ODER c$.
+Damit folgt $(a UND [b ODER c])$.
+qed.
+Damit folgt $(a UND b) -> (a UND [b ODER c])$.
+Fall 2.
+Angenommen nun es gilt $a UND c$.
+Dann gilt a.
+Ferner gilt c.
+Also gilt auch $b ODER c$.
+Damit folgt $a UND (b ODER c)$.
+qed.
+Damit folgt $(a UND c) -> (a UND [b ODER c])$.
+In jedem fall gilt $(a UND [b ODER c])$.
+Also gilt $(a UND [b ODER c])$.
+qed.
+Also gilt $([a UND b] ODER [a UND c]) -> (a UND [b ODER c])$.
+Damit folgt nun endlich $(a UND [b ODER c]) <-> ([a UND b] ODER [a UND c])$.
+qed.};*/
