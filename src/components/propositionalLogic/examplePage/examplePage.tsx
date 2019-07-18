ab9677064da438397d6f2abf66d42b1fe7d6ae8e
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-18 20:30:51 +0200

    minor changes

--- src/components/propositionalLogic/examplePage/examplePage.tsx
+++ src/components/propositionalLogic/examplePage/examplePage.tsx
@@ -1,34 +1,64 @@
 import React from "react";
 
+import Issue from "../../../util/issueHandling/issue";
+import { checkProof } from "../../../util/proofChecker";
+import IssueInformation from "../issueInformation";
 import ProofEditor from "../proofEditor";
 import styles from "../propositionalLogic.module.scss";
 import { Example, generateRandomExample } from "./exampleCollection";
 
-const lyrics = "placeholder";
+interface State {
+	userInput: string;
+	issues: readonly Issue[];
+}
 
-class ExamplesPropositionalLogic extends React.Component {
-
+class ExamplesPropositionalLogic extends React.Component<{}, State> {
+	public state = {
+		userInput: "",
+		issues: [],
+	};
 	public render() {
 		return <div className={styles.site}>
 			<div>
 				<p>Hier sollten Beispiele stehen. Entweder Zufällig generierte bzw. ausgewählte Aufgaben (nach schiwerigkeitsgrad?)
 				 oder festes schema)</p>
 				<p>Click the button to display a random example.</p>
+
+				<p id="exampleDescription"></p>
+				<div className={ProofEditor.name}>
+				</div>
+				<p id="exampleContent"></p>
+			</div>
+			<div className={styles.proofEditor}>
+				<textarea
+					className={styles.textInput}
+					placeholder={textboxPlaceHolder}
+					value={this.state.userInput}
+					onChange={ev => this.setState({ userInput: ev.target.value })} />
 				<button className={styles.buttons}
 					onClick={this.button}>
 					Nächstes Beispiel
-
 					</button>
+				<button className={styles.buttons}
+					onClick={this.checkInput}>
+					Prüfen
+				</button>
+				<div className={styles.issuesInformation}>
+					{this.state.issues.map((issue: Issue) => {
+						return <IssueInformation
+							issue={issue} />;
 
-				<p id="exampleDescription"></p>
-					<div className={ProofEditor.name}>
-					</div>
-					<p id="exampleContent"></p>
+					})}
+				</div>
 			</div>
-			<ProofEditor/>
-			<div>{lyrics}</div>
-		</div >;
+		</div>;
+	}
 
+	private readonly checkInput = async (): Promise<void> => {
+		const issueArray: readonly Issue[] = await checkProof(this.state.userInput);
+		//const randomExample: Example = generateRandomExample();
+
+		this.setState({ issues: issueArray });
 	}
 	private button = async (): Promise<void> => {
 		const exampleDiscription = document.getElementById("exampleDescription");
@@ -36,9 +66,17 @@ class ExamplesPropositionalLogic extends React.Component {
 		const randomExample: Example = generateRandomExample();
 		if (exampleDiscription !== null && exampleContent !== null) {
 			exampleDiscription.innerHTML = randomExample.exampleDiscription;
-			exampleContent.innerHTML = randomExample.exampleContent;
+			// exampleContent.innerHTML = randomExample.exampleContent;
+			updateInputPlaceholder(randomExample.exampleContent);
 		}
 	}
+}
 
+const textboxPlaceHolder = "Gebe hier deinen Beweis ein...";
+let textBoxValue = "";
+
+export function updateInputPlaceholder(newPlaceholder: string): string {
+	textBoxValue = newPlaceholder;
+	return textBoxValue;
 }
 export default ExamplesPropositionalLogic;
