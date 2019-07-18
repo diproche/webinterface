51a16c29a875e96b52f08746fe543db1fdc6ab78
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-07-18 10:30:12 +0200

    Add test for multiple insertions (fails)

--- src/util/tests/addStringIgnoringHTML.test.ts
+++ src/util/tests/addStringIgnoringHTML.test.ts
@@ -4,7 +4,7 @@ describe("Normal Cases", () => {
 	test("Inserts Into A Normal String", () => {
 		const basisString: string = "This is test";
 		const stringToAdd: string = " a";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 7);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 7);
 		expect(stringWithInsertion).toStrictEqual("This is a test");
 	});
 
@@ -12,29 +12,36 @@ describe("Normal Cases", () => {
 		const basisString: string = "<textarea /> This is textarea";
 		const stringToAdd: string = " a";
 				// Inserting at index 8 because of the whitespace between the tag and the actual text
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 8);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 8);
 		expect(stringWithInsertion).toStrictEqual("<textarea /> This is a textarea");
 	});
 
 	test("Ignores Multiple HTML Tags", () => {
 		const basisString: string = "<div><u>This</u> is <b>test</b></div>";
 		const stringToAdd: string = " a";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 7);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 7);
 		expect(stringWithInsertion).toStrictEqual("<div><u>This</u> is a <b>test</b></div>");
 	});
 
 	test("Avoids Doubling Up Strings As Default", () => {
 		const basisString: string = "This is a test";
 		const stringToAdd: string = " a";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 7);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 7);
 		expect(stringWithInsertion).toStrictEqual("This is a test");
 	});
 
 	test("Doubles Up Strings If Specified", () => {
 		const basisString: string = "This is a test";
 		const stringToAdd: string = " a";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 7, true);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 7, true);
 		expect(stringWithInsertion).toStrictEqual("This is a a test");
+	});
+
+	test("Will add the same string in multiple locations", () => {
+		const basisString: string =  "Wenn gleich ist, dann";
+		const stringToAdd: string = " X";
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, [4, 11, 21]);
+		expect(stringWithInsertion).toStrictEqual("Wenn X gleich X ist, dann X");
 	});
 });
 
@@ -42,42 +49,42 @@ describe("Limit Cases", () => {
 	test("Will Add To The Beginning Of the String With Position = 0", () => {
 		const basisString: string = "is a test";
 		const stringToAdd: string = "This ";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 0);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 0);
 		expect(stringWithInsertion).toStrictEqual("This is a test");
 	});
 
 	test("Will Add To The End Of the String With Position = basisString.length + 1", () => {
 		const basisString: string = "This is a ";
 		const stringToAdd: string = "test";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, basisString.length + 1);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, basisString.length + 1);
 		expect(stringWithInsertion).toStrictEqual("This is a test");
 	});
 
 	test("If The Position Is At A Closing HTML Tag It Inserts Outside The HTML Tag", () => {
 		const basisString: string = "This is <b>a</b>";
 		const stringToAdd: string = " test";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, basisString.length);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, basisString.length);
 		expect(stringWithInsertion).toStrictEqual("This is <b>a</b> test");
 	});
 
 	test("If The Position Is At A Opening HTML Tag It Inserts Inside The HTML Tag", () => {
 		const basisString: string = "This <b>a</b> test";
 		const stringToAdd: string = "is ";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 5);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 5);
 		expect(stringWithInsertion).toStrictEqual("This is <b>a</b> test");
 	});
 
 	test("< Doesn't Break It", () => {
 		const basisString: string = "5 < 3";
 		const stringToAdd: string = "5";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 5);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 5);
 		expect(stringWithInsertion).toStrictEqual("5 < 35");
 	});
 
 	test("< [...] > Doesn't Break It", () => {
 		const basisString: string = "5 < 3 and 35 > 5";
 		const stringToAdd: string = "5";
-		const stringWithInsertion = addStringIgnoringHTML(basisString, stringToAdd, 5);
+		const stringWithInsertion: string = addStringIgnoringHTML(basisString, stringToAdd, 5);
 		expect(stringWithInsertion).toStrictEqual("5 < 35 and 35 > 5");
 	});
 });
