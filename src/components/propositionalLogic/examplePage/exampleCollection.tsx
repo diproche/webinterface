397f210aed72731a4b138674ec597504d8bf1f97
RonjaKaehne <49696227+RonjaKaehne@users.noreply.github.com>
2019-07-19 17:51:22 +0200

    minor changes

--- src/components/propositionalLogic/examplePage/exampleCollection.tsx
+++ src/components/propositionalLogic/examplePage/exampleCollection.tsx
@@ -1,95 +1,23 @@
+export class Example {
+	public exampleDiscription!: string;
+	public exampleContent!: string;
+	public exampleConclusion!: string;
+}
+
 /**
  * generate a random example object including a discription and the generated example-content
  * @return the example object
  */
-export function generateRandomExample(): Example {
-	const exampleClassNumber: number = exampleRandomClassNumberGenerator();
-	const exampleDiscription = matchExampleDiscription(exampleClassNumber);
-	const exampleContent = generateExampleContent(exampleClassNumber);
-	const example: Example = new Example();
-	example.exampleDiscription = exampleDiscription;
-	example.exampleContent = exampleContent;
+export function pickExample(): Example {
+	let example: Example;
+	example = new Example();
+	example.exampleDiscription = "Es seien a, b und c Aussagen. " +
+									"Angenommen a ->(b -> c) ";
+	example.exampleContent =		"Angenommen ferner es gilt (a und b) " +
+									"Dann folgt a. " +
+									"Ausserdem folgt b. " +
+									"Damit gilt (b -> c). " +
+									"Ferner folgt c. ";
+	example.exampleConclusion = 	"Also gilt $a -> (b -> c) -> (a und b) -> c$. ";
 	return example;
 }
-
-/**
- * generates a random example class
- * @return the number of the example class
- */
-export function exampleRandomClassNumberGenerator(): number {
-	const exampleClassNumber: number = Math.floor(Math.random() * 2);
-	return exampleClassNumber;
-}
-
-export class Example {
-	public exampleDiscription!: string;
-	public exampleContent!: string;
-}
-
-enum exampleDiscriptions {
-	proofAllTrue = "Zeigen Sie, dass folgender Ausdruck immer wahr wird:",
-	proofAllFalse = "Zeigen Sie, dass folgender Ausdruck immer falsch wird:",
-}
-
-function matchExampleDiscription(exampleClassNumber: number): string {
-	const discriptions = [exampleDiscriptions.proofAllFalse, exampleDiscriptions.proofAllTrue];
-	const exampleDiscription = discriptions[exampleClassNumber];
-	return exampleDiscription;
-}
-
-function generateExampleContent(exampleClassNumber: number): string {
-	const test = exampleClassNumber.toString();
-	return test;
-}
-
-/*
-const example = {"
-Wir zeigen (a UND [b ODER c]) <-> ([a UND b] ODER [a UND c]).
-
-=>.
-Angenommen es gilt $(a UND [b ODER c])$.
-Dann gilt a.
-Ferner gilt $b ODER c$.
-Fallunterscheidung.
-Fall 1.
-Angenommen es gilt b.
-Dann gilt $a UND b$.
-Also folgt $([a UND b] ODER [a UND c])$.
-qed.
-Damit gilt $b -> ([a UND b] ODER [a UND c])$.
-Fall 2.
-Angenommen es gilt c.
-Dann gilt $a UND c$,
-Also folgt $([a UND b] ODER [a UND c])$.
-qed.
-Damit gilt $c -> ([a UND b] ODER [a UND c])$.
-In jedem Fall gilt $([a UND b] ODER [a UND c])$.
-Also gilt $([a UND b] ODER [a UND c])$.
-qed.
-Also gilt $([a UND b] ODER [a UND c]) -> (a UND [b ODER c])$.
-
-<=.
-Angenommen es gilt $([a UND b] ODER [a UND c])$.
-Fallunterscheidung.
-Fall 1.
-Angenommen es gilt $a UND b$.
-Dann gilt a.
-Ferner gilt b.
-Also gilt auch $b ODER c$.
-Damit folgt $(a UND [b ODER c])$.
-qed.
-Damit folgt $(a UND b) -> (a UND [b ODER c])$.
-Fall 2.
-Angenommen nun es gilt $a UND c$.
-Dann gilt a.
-Ferner gilt c.
-Also gilt auch $b ODER c$.
-Damit folgt $a UND (b ODER c)$.
-qed.
-Damit folgt $(a UND c) -> (a UND [b ODER c])$.
-In jedem fall gilt $(a UND [b ODER c])$.
-Also gilt $(a UND [b ODER c])$.
-qed.
-Also gilt $([a UND b] ODER [a UND c]) -> (a UND [b ODER c])$.
-Damit folgt nun endlich $(a UND [b ODER c]) <-> ([a UND b] ODER [a UND c])$.
-qed.};*/
--- src/components/propositionalLogic/examplePage/examplePage.tsx
+++ src/components/propositionalLogic/examplePage/examplePage.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import Issue from "../../../util/issueHandling/issue";
 import { checkProof } from "../../../util/proofChecker";
 import IssueInformation from "../issueInformation";
-import ProofEditor from "../proofEditor";
 import styles from "../propositionalLogic.module.scss";
-import { Example, generateRandomExample } from "./exampleCollection";
+import { Example, pickExample } from "./exampleCollection";
 
 interface State {
 	userInput: string;
@@ -23,11 +22,13 @@ class ExamplesPropositionalLogic extends React.Component<{}, State> {
 				<p>Hier sollten Beispiele stehen. Entweder Zufällig generierte bzw. ausgewählte Aufgaben (nach schiwerigkeitsgrad?)
 				 oder festes schema)</p>
 				<p>Click the button to display a random example.</p>
-
+				<button className={styles.buttons}
+					onClick={this.button}>
+					Nächstes Beispiel
+					</button>
 				<p id="exampleDescription"></p>
-				<div className={ProofEditor.name}>
-				</div>
 				<p id="exampleContent"></p>
+
 			</div>
 			<div className={styles.proofEditor}>
 				<textarea
@@ -35,10 +36,7 @@ class ExamplesPropositionalLogic extends React.Component<{}, State> {
 					placeholder={textboxPlaceHolder}
 					value={this.state.userInput}
 					onChange={ev => this.setState({ userInput: ev.target.value })} />
-				<button className={styles.buttons}
-					onClick={this.button}>
-					Nächstes Beispiel
-					</button>
+					<p id="exampleConclusion"></p>
 				<button className={styles.buttons}
 					onClick={this.checkInput}>
 					Prüfen
@@ -56,18 +54,19 @@ class ExamplesPropositionalLogic extends React.Component<{}, State> {
 
 	private readonly checkInput = async (): Promise<void> => {
 		const issueArray: readonly Issue[] = await checkProof(this.state.userInput);
-		//const randomExample: Example = generateRandomExample();
 
 		this.setState({ issues: issueArray });
 	}
 	private button = async (): Promise<void> => {
 		const exampleDiscription = document.getElementById("exampleDescription");
 		const exampleContent = document.getElementById("exampleContent");
-		const randomExample: Example = generateRandomExample();
-		if (exampleDiscription !== null && exampleContent !== null) {
+		const exampleConclusion = document.getElementById("exampleConclusion");
+		const randomExample: Example = pickExample();
+		if (exampleDiscription !== null && exampleContent !== null && exampleConclusion !== null) {
 			exampleDiscription.innerHTML = randomExample.exampleDiscription;
-			// exampleContent.innerHTML = randomExample.exampleContent;
-			updateInputPlaceholder(randomExample.exampleContent);
+			exampleContent.innerHTML = randomExample.exampleContent;
+			exampleConclusion.innerHTML = randomExample.exampleConclusion;
+
 		}
 	}
 }
