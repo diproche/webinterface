93f553afc05ef05a270ff83b956743833c124bf4
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-07-27 00:37:59 +0200

    Enable display of eplanation by clicking

--- src/components/propositionalLogic/examplePage.tsx
+++ src/components/propositionalLogic/examplePage.tsx
@@ -2,18 +2,35 @@ import React from "react";
 
 import styles from "./propositionalLogic.module.scss";
 
-class ExamplesPropositionalLogic extends React.Component {
+interface State {
+	proofPart: Array<[string, string, boolean]>;
+}
+
+class ExamplesPropositionalLogic extends React.Component<{}, State> {
+
+	// Via constructor since an immediate assignment led to type issues
+	constructor(props: {}) {
+		super(props);
+		const proofPart: Array<[string, string, boolean]> = [
+			["Es seien a, b und c Aussagen.", "Allgemeine Vorraussetzung", false],
+			["Angenommen $a ->(b -> c)$.", "Vorraussetzung: Die Prämisse a -> (b -> c) ist wahr.", false],
+			["Angenommen ferner es gilt $(a und b)$.", "Vorraussetzung: (a und b) ist wahr.", false],
+			["Dann folgt a.", "a folgt, da (a und b) nur dann wahr ist, wenn a wahr ist.", false],
+			["Ausserdem folgt b.", "b folgt, da (a und b) nur dann wahr ist, wenn b wahr ist.", false],
+			["Damit gilt $(b -> c)$.", "Die Konlusion (b -> c) gilt, da die Prämissen a,sowie (a und b) beide gelten.", false],
+			["Ferner folgt c.", "Die Konlusion c gilt, da die Prämissen a, sowie (a und b) beide gelten.", false],
+		];
+
+		this.state = { proofPart };
+	}
+
 	public render() {
 		return <div className={styles.site}>
-			<div className={styles.proofEditor}>
-				<p><button className={styles.buttons}
-					onClick={this.showElement}>
-					Beispiel 1
-						</button></p>
-				<table className={styles.toggleTable} id="toggleTable">
+			<table className={styles.toggleTable}>
+				<tbody>
 					<tr>
 						<th>Eingabe</th>
-						<th>Erklärung (mit der Maus drüberfahren zum anzeigen)</th>
+						<th></th>
 					</tr>
 					<tr>
 						<th>Wir zeigen: a -> (b -> c) -> (a und b) -> c.</th>
@@ -23,35 +40,7 @@ class ExamplesPropositionalLogic extends React.Component {
 						<td></td>
 						<td></td>
 					</tr>
-					<tr>
-						<td>Es seien a, b und c Aussagen.</td>
-						<td className={styles.element}> Allgemeine Vorraussetzung</td>
-					</tr>
-					<tr>
-						<td>Angenommen $a ->(b -> c)$.</td>
-						<td className={styles.element}>Vorraussetzung: Die Prämisse a -> (b -> c) ist wahr. </td>
-					</tr>
-					<tr>
-						<td>Angenommen ferner es gilt $(a und b)$.</td>
-						<td className={styles.element}>Vorraussetzung: (a und b) ist wahr.</td>
-					</tr>
-					<tr>
-						<td>Dann folgt a.</td>
-						<td className={styles.element}>a folgt, da (a und b) nur dann wahr ist, wenn a wahr ist. </td>
-					</tr>
-					<tr>
-						<td>Ausserdem folgt b.</td>
-						<td className={styles.element}>b folgt, da (a und b) nur dann wahr ist, wenn b wahr ist. </td>
-					</tr>
-					<tr>
-						<td>Damit gilt $(b -> c)$.</td>
-						<td className={styles.element}> Die Konlusion (b -> c) gilt, da die Prämissen a,
-						sowie (a und b) beide gelten.</td>
-					</tr>
-					<tr>
-						<td>Ferner folgt c.</td>
-						<td className={styles.element}> Die Konlusion c gilt, da die Prämissen a, sowie (a und b) beide gelten. </td>
-					</tr>
+					{this.renderProof()}
 					<tr>
 						<td></td>
 						<td></td>
@@ -60,19 +49,45 @@ class ExamplesPropositionalLogic extends React.Component {
 						<td>Also gilt $a -> (b -> c) -> (a und b) -> c$.</td>
 						<td>qed </td>
 					</tr>
-				</table>
-
-			</div>
-
-		</div >;
+				</tbody>
+			</table>
+		</div>;
 	}
 
-	private showElement = async (): Promise<void> => {
-		const myComponent = document.getElementById("toggleTable");
-		if (myComponent !== null) {
-			myComponent.style.display = "block";
+	private renderProof(): JSX.Element {
+		const renderedPart: JSX.Element[] = [];
+
+		this.state.proofPart.forEach((line: [string, string, boolean], index: number) => {
+			let explanation: string;
+			let scssClass: string;
+			if (line[2]) {
+				explanation = line[1];
+				scssClass = styles.alreadyClicked;
+			} else {
+				explanation = "Klicke hier um eine Erklärung anzuzeigen";
+				scssClass = styles.clickToChange;
+			}
+
+			renderedPart.push(<tr>
+				<td>{line[0]}</td>
+				<td className={scssClass} onClick={() => this.showExplanation(index)}>{explanation}</td>
+			</tr>);
+		});
+
+		return <React.Fragment>
+			{renderedPart}
+		</React.Fragment>;
+	}
+
+	private showExplanation(index: number): void {
+		if (!this.state.proofPart[index][2]) {
+			const copy: Array<[string, string, boolean]> = this.state.proofPart;
+			copy[index][2] = true;
+			this.setState({proofPart: copy});
 		}
+
 	}
+
 }
 
 export default ExamplesPropositionalLogic;
