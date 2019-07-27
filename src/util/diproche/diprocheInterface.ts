70acc9838576422a717bdfdc89a14f06b11de1b9
Kevin Weirauch <kweirauch@uni-koblenz.de>
2019-07-27 12:12:45 +0200

    Add documentation in diprocheInterface.ts

--- src/util/diproche/diprocheInterface.ts
+++ src/util/diproche/diprocheInterface.ts
@@ -9,29 +9,48 @@ export enum Mode {
 	test = "teste",
 }
 
+/**
+ * returns a string containing the userInput as an argument of a predicate
+ * @param userInput - the user input
+ * @param mode - the mode
+ */
 export function addPredicate(userInput: string, mode: Mode) {
 	return mode + "(" + userInput + ").";
 }
 
+/**
+ * collects all vocabErrors
+ */
 export function getVocabErrors(userInput: string): void {
 	collectInvalidWordsInIssues(userInput);
 }
 
+/**
+ * collects all vocabErrors and should also collect grammatical errors
+ */
 export function getSyntacticErrors(userInput: string) {
 	getVocabErrors(userInput);
 }
 
+/**
+ * Collects all errors before the execution of diproche
+ */
 export function getErrorsBeforeDiproche(userInput: string) {
 	getSyntacticErrors(userInput);
 	// getSemanticErrors(userInput);
 }
 
+/**
+ * Collects all errors that diproche returns
+ */
 export async function getErrorsAfterDiproche(diprocheInput: string): Promise<void> {
 	const response: string = await getDiprocheResponse(diprocheInput);
 	addDiprocheIssues(response);
 }
 
-// this function collects all Errors.
+/**
+ * this function collects all Errors.
+ */
 export async function createErrors(userinput: string): Promise<void> {
 	getErrorsBeforeDiproche(userinput);
 
