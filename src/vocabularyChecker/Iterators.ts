c5ddf6e8f866b2015c0489b9139e72653c9b6549
Kevin Weirauch <kweirauch@uni-koblenz.de>
2019-07-27 12:15:59 +0200

    Add documentation in iterator.ts

--- src/vocabularyChecker/Iterators.ts
+++ src/vocabularyChecker/Iterators.ts
@@ -1,3 +1,7 @@
+/**
+ * Returns a new iterable that contains no duplicate values
+ * @param values - some iterable in which duplicates should be removed
+ */
 export default function uniqueValues<T>(values: Iterable<T>): Iterable<T> {
 		return new Set(values);
 	}
