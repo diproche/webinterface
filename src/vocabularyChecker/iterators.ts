a48fc8d044b3bad75608a81dd77d6aa1fe60a2ed
Kevin Weirauch <kweirauch@uni-koblenz.de>
2019-07-28 09:16:46 +0200

    Remove duplicate of vocabularyChecker

--- src/vocabularyChecker/iterators.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns a new iterable that contains no duplicate values
- * @param values - some iterable in which duplicates should be removed
- */
-export default function uniqueValues<T>(values: Iterable<T>): Iterable<T> {
-		return new Set(values);
-}
