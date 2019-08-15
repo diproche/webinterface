732029963464e0a2de1d57e798686507ec692a49
Kevin Weirauch <kweirauch@uni-koblenz.de>
2019-08-15 12:50:57 +0200

    Add missing whitespace

--- src/components/propositionalLogic/wiki.tsx
+++ src/components/propositionalLogic/wiki.tsx
@@ -38,7 +38,7 @@ class WikiPropositionalLogic extends React.Component {
 					F heißt nun <b>allgemeingültig</b> oder auch <b>Tatutologie</b>, falls jede Wertebelegung F wahr macht.
 					Gibt es mindestens eine Wahrheitswertebelegung, sodass F wahr wird, so heißt
 					F <b>erfüllbar</b>. Gibt es keine solche Wahrheitswertebelegung, dann heißt F
-					<b>unerfüllbar</b> oder auch <b>Kontradiktion</b>.
+					 <b>unerfüllbar</b> oder auch <b>Kontradiktion</b>.
 				</p>
 				<h2>Was ist ein Beweis?</h2>
 				Wenn man etwas beweisen muss, so handelt es sich immer um eine Aussage.
