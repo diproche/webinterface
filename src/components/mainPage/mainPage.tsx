b3aa2f34b3bea870043acae3958f44581ae36f0e
Kevin Weirauch <kweirauch@uni-koblenz.de>
2019-08-17 14:44:47 +0200

    Add first version of main page text

--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -4,7 +4,17 @@ class ProofEditor extends React.Component {
 
 	public render() {
 		return <div className={"addStyleHere"}>
-			<p>Willkommen auf unserer Startseite. Das hier sieht noch sehr nach Baustelle aus...</p>
+			<h1>Willkommen bei Diproche!</h1>
+			<p>"Diproche" steht für "Didactical proof checking". Das heißt im Wesentlichen, dass Diproche die
+				Möglichkeit bietet, Beweise auf ihre Richtigkeit zu überprüfen. Dafür müssen die Beweise auf eine
+				bestimmte Art aufgeschrieben werden. Wie man das macht, kann man in der Kategorie "Beispiele" sehen.
+				Unter "Übungen" gibt es vorgefertigte Aufgabenstellungen, die man durch
+				einen Beweis lösen soll. Will man eine eigene Aussage formulieren und beweisen,
+				kann man die Kategorie "Freies Üben" dafür nutzen. Wenn man noch nicht
+				allzu vertraut mit der Theorie hinter dem ist, was man beweisen möchte, findet
+				man im "Wiki" einige Informationen dazu. Bei Problemen befinden sich unter "Kontakt"
+				die passenden Ansprechpartner. Viel Spaß beim Beweisen!
+			</p>
 		</div>;
 
 	}
