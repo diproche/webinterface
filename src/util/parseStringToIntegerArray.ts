3ee6dd77ba4a20371a9d14a0b9620f660c257023
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2019-08-27 21:35:42 +0200

    Utilize the more efficient suggestion

--- src/util/parseStringToIntegerArray.ts
+++ src/util/parseStringToIntegerArray.ts
@@ -1,30 +1,25 @@
 /**
 	* Converts a string to an array of numbers
-	* @param integerArrayString - A string representing a array of integers (i.e. [16, 87, 27])
+	* @param integerArrayAsString - A string representing a array of integers (i.e. [16, 87, 27])
 	* @return An array of integers represented by the string
 	*/
-export function parseStringToIntegerArray(integerArrayString: string) {
+export function parseStringToIntegerArray(integerArrayAsString: string) {
 
-	if (integerArrayString === "[]") {
+	if (integerArrayAsString === "[]") {
 		return [];
 	}
 
 	if (
-		integerArrayString.substr(0, 1) !== "[" ||
-		integerArrayString.substr(integerArrayString.length - 1, 1) !== "]"
+		integerArrayAsString.substr(0, 1) !== "[" ||
+		integerArrayAsString.substr(integerArrayAsString.length - 1, 1) !== "]"
 	) {
 		throw new Error("The given string is not contained in brackets '[]'");
 	}
 
 	// Removing the outter brackets
-	integerArrayString = integerArrayString.substr(1, integerArrayString.length - 2);
+	integerArrayAsString = integerArrayAsString.substr(1, integerArrayAsString.length - 2);
 
-	const arrayOfStrings: string[] = integerArrayString.split(",");
-	const arrayOfInteger: number[] = [];
-
-	arrayOfStrings.forEach((numberString: string) => {
-		arrayOfInteger.push(Number.parseInt(numberString, 10));
-	});
+	const arrayOfInteger = integerArrayAsString.split(",").map(s => Number.parseInt(s, 10));
 
 	return arrayOfInteger;
 
